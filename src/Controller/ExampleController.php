32bcbe3103364cd2728f3356e1133508dd72119b
lucas01062003 <lucas.hermus@student.gildeopleidingen.nl>
2024-05-14 11:25:22 +0200

    Model and controller examples

---
+++ src/Controller/ExampleController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\src\Controller;
+
+class ExampleControllerController
+{
+    public function index()
+    {
+        view("Pages/index.html");
+    }
+}
---
+++ src/Model/ExampleModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\src\Model;
+
+use App\Core\Database\Model;
+
+class ExampleModel extends Model
+{
+    public $table = "example";
+}
